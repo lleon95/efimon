30541f31be397887b15ac3268bb550c90c691589
Luis Leon <lleon95@gmail.com>
2023-11-20 14:23:12 +0100

    Add the status struct
    The status struct helps in determining the status codes and the throw
    objects according to std::exception

---
+++ include/efimon/status.hpp
@@ -0,0 +1,62 @@
+/**
+ * @file status.hpp
+ * @author Luis G. Leon-Vega (luis.leon@ieee.org)
+ * @brief Encapsulates the message
+ *
+ * @copyright Copyright (c) 2023. See License for Licensing
+ */
+
+#ifndef INCLUDE_EFIMON_STATUS_HPP_
+#define INCLUDE_EFIMON_STATUS_HPP_
+
+#include <exception>
+#include <string>
+
+namespace efimon {
+
+/**
+ * @brief Status structure
+ *
+ * Class that encapsulates the error codes and works for exceptions
+ */
+class Status : public std::exception {
+ public:
+  /** Error codes */
+  enum {
+    OK = 0,            /** OK Status */
+    FILE_ERROR,        /** File error that can be read or write */
+    INVALID_PARAMETER, /** Invalid argument or parameter. i.e. nullptr */
+    /** Incompatible parameter that it is not supported
+                              by a function */
+    INCOMPATIBLE_PARAMETER,
+    CONFIGURATION_ERROR, /** Configuration error*/
+    REGISTER_IO_ERROR,   /** Register MMIO error */
+    NOT_IMPLEMENTED,     /** Not implemented error */
+    MEMBER_ABSENT,       /** Missing member */
+    RESOURCE_BUSY,       /** Busy */
+    NOT_FOUND,           /** Reosurce not found */
+  };
+
+  /**
+   * @brief Construct a new Status object
+   *
+   * It default the error to be 0 or OK
+   */
+  Status() noexcept : std::exception{}, code{0} {}
+
+  /**
+   * @brief Construct a new Status object
+   *
+   * It defines the constructor to define a custom code and description
+   *
+   * @param code code of the error
+   * @param msg description
+   */
+  Status(const int code, const std::string &msg) noexcept
+      : std::exception{msg}, code{code} {}
+
+  /** Error code for C compatibility */
+  int code;
+};
+} /* namespace efimon */
+#endif /* INCLUDE_EFIMON_STATUS_HPP_ */
