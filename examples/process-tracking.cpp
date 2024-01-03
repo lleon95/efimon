d42c4830b9efd7aff19096312d5d23477665c581
Luis Leon <lleon95@gmail.com>
2024-01-03 23:51:16 +0100

    Integrate timing to the example
    Now, it is capable of monitoring the new and dead processes

--- examples/process-tracking.cpp
+++ examples/process-tracking.cpp
@@ -6,6 +6,8 @@
  * @copyright Copyright (c) 2024. See License for Licensing
  */
 
+#include <unistd.h>
+
 #include <algorithm>
 #include <iomanip>
 #include <iostream>
@@ -22,27 +24,42 @@ int main(int argc, char **argv) {
     for (int i = 1; i < argc; ++i) users.emplace_back(argv[i]);
   }
 
-  /* Monitor the processes */
   efimon::ProcPsProcessLister plister;
-  plister.Detect();
+  for (int i = 0; i < 30; ++i) {
+    /* Monitor the processes */
+    plister.Detect();
 
-  auto new_list = plister.GetNew();
-  auto dead_list = plister.GetDead();
-  auto last_list = plister.GetLast();
+    auto new_list = plister.GetNew();
+    auto dead_list = plister.GetDead();
+    auto last_list = plister.GetLast();
 
-  std::cout << "Vector sizes:" << std::endl;
-  std::cout << "\tNew: " << new_list.size() << std::endl;
-  std::cout << "\tDead: " << dead_list.size() << std::endl;
-  std::cout << "\tLast: " << last_list.size() << std::endl;
-
-  std::cout << "Processes Running:" << std::endl;
-  std::cout << "PID\t" << std::setw(20) << "CMD\tOwner" << std::endl;
-  for (auto &ps : last_list) {
-    bool found = std::find(users.begin(), users.end(), ps.owner) != users.end();
-    bool all = users.size() == 0;
-    if (found || all)
-      std::cout << ps.pid << "\t" << std::setw(20) << ps.cmd << "\t" << ps.owner
+    if (dead_list.size() > 0) {
+      std::cout << "Processes Dead:" << std::endl;
+      std::cout << "PID" << std::setw(30) << "CMD" << std::setw(10) << "Owner"
                 << std::endl;
+      for (auto &ps : dead_list) {
+        bool found =
+            std::find(users.begin(), users.end(), ps.owner) != users.end();
+        bool all = users.size() == 0;
+        if (found || all)
+          std::cout << ps.pid << std::setw(30) << ps.cmd << std::setw(10)
+                    << ps.owner << std::endl;
+      }
+    }
+    if (new_list.size() > 0) {
+      std::cout << "Processes New:" << std::endl;
+      std::cout << "PID" << std::setw(30) << "CMD" << std::setw(10) << "Owner"
+                << std::endl;
+      for (auto &ps : new_list) {
+        bool found =
+            std::find(users.begin(), users.end(), ps.owner) != users.end();
+        bool all = users.size() == 0;
+        if (found || all)
+          std::cout << ps.pid << std::setw(30) << ps.cmd << std::setw(10)
+                    << ps.owner << std::endl;
+      }
+    }
+    sleep(1);
   }
 
   return 0;
