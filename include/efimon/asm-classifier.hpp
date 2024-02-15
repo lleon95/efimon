48279aa177987b7727b42b7d1435698d6cb6de54
Luis Leon <lleon95@gmail.com>
2024-02-15 12:03:07 +0100

    Add assembly classifier interface and enums
    Defines the infrastructure to parse the assembly nmemonics.

---
+++ include/efimon/asm-classifier.hpp
@@ -0,0 +1,88 @@
+/**
+ * @file asm-classifier.hpp
+ * @author Luis G. Leon-Vega (luis.leon@ieee.org)
+ * @brief This header contains mappings and helpers to decypher the type
+ * of ASM instruction executed within the system
+ *
+ * @copyright Copyright (c) 2024. See License for Licensing
+ */
+
+#ifndef INCLUDE_EFIMON_ASM_CLASSIFIER_HPP_
+#define INCLUDE_EFIMON_ASM_CLASSIFIER_HPP_
+
+#include <string>
+#include <utility>
+
+namespace efimon {
+namespace assembly {
+
+/**
+ * Enumerator for the instruction family, classifying it according to its
+ * functionality
+ */
+enum class InstructionFamily {
+  /** Arithmetic: add, sub, div, mul */
+  ARITHMETIC = 0,
+  /** Logic: and, or, not, xor, shift, mask */
+  LOGIC,
+  /** Memory: mov, load, store */
+  MEMORY,
+  /** Conditional branching: test, jz.. */
+  BRANCH,
+  /** Unconditional branching: jmp, jump... */
+  JUMP,
+  /** Other not absorbed above*/
+  OTHER
+};
+
+/**
+ * Enumerator for the instruction type, classifying according to its SIMDness
+ */
+enum class InstructionType {
+  /** Scalar instructions: without any kind of vectorisation */
+  SCALAR = 0,
+  /** Vector instructions: including matrix and vector */
+  VECTOR,
+  /** Unclassified instructions, like stacking pop and push */
+  UNCLASSIFIED
+};
+
+}  // namespace assembly
+
+/**
+ * Type to return the type and family in a single run
+ */
+using InstructionPair =
+    std::pair<assembly::InstructionType, assembly::InstructionFamily>;
+
+/**
+ * Interface to classify the instructions into families and types
+ */
+class AsmClassifier {
+ public:
+  /**
+   * Classifies the instruction from string to InstructionType and
+   * InstructionFamily
+   */
+  virtual InstructionPair Classify(const std::string &inst) const noexcept = 0;
+
+  /**
+   * Default destructor for inheritance (implementation)
+   */
+  virtual ~AsmClassifier() = default;
+
+  /**
+   * Gets the string from the family enumerator (InstructionFamily)
+   */
+  static const std::string FamilyString(
+      const assembly::InstructionFamily family);
+
+  /**
+   * Gets the string from the type enumerator (InstructionType)
+   */
+  static const std::string TypeString(const assembly::InstructionType type);
+};
+
+} /* namespace efimon */
+
+#endif  // INCLUDE_EFIMON_ASM_CLASSIFIER_HPP_
---
+++ src/efimon/asm-classifier.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file asm-classifier.cpp
+ * @author Luis G. Leon-Vega (luis.leon@ieee.org)
+ * @brief This header contains mappings and helpers to decypher the type
+ * of ASM instruction executed within the system
+ *
+ * @copyright Copyright (c) 2024. See License for Licensing
+ */
+
+#include <efimon/asm-classifier.hpp>
+#include <string>
+
+namespace efimon {
+
+const std::string AsmClassifier::FamilyString(
+    const assembly::InstructionFamily family) {
+  switch (family) {
+    case assembly::InstructionFamily::ARITHMETIC:
+      return "Arithmetic";
+    case assembly::InstructionFamily::LOGIC:
+      return "Logic";
+    case assembly::InstructionFamily::MEMORY:
+      return "Memory";
+    case assembly::InstructionFamily::BRANCH:
+      return "Branch";
+    case assembly::InstructionFamily::JUMP:
+      return "Jump";
+    default:
+      return "Other";
+  }
+}
+
+const std::string AsmClassifier::TypeString(
+    const assembly::InstructionType type) {
+  switch (type) {
+    case assembly::InstructionType::SCALAR:
+      return "Scalar";
+    case assembly::InstructionType::VECTOR:
+      return "Vector";
+    default:
+      return "Unclassified";
+  }
+}
+
+} /* namespace efimon */
