abad6e4809a0282ac7b58ed837c3f0b0d7efb86f
Luis Leon <lleon95@gmail.com>
2024-06-08 21:21:24 +0200

    Add skeleton for NVIDIA NVML
    This adds the dependency and the source skeleton

---
+++ src/efimon/gpu/nvidia.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file nvidia.cpp
+ * @author Luis G. Leon-Vega (luis.leon@ieee.org)
+ * @brief NVIDIA GPU analyser: consumption in RAM, usage and power (if
+ * available)
+ *
+ * @copyright Copyright (c) 2024. See License for Licensing
+ */
+
+#include <efimon/gpu/nvidia.hpp>
+#include <efimon/observer.hpp>
+#include <efimon/readings.hpp>
+#include <efimon/readings/gpu-readings.hpp>
+#include <vector>
+
+namespace efimon {} /* namespace efimon */
