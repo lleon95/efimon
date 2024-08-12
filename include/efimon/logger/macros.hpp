29864e563a0776c308f10c6dffa01dea0d211d9b
Luis Leon <lleon95@gmail.com>
2024-08-12 13:56:21 +0200

    Add daemon argument parsing
    This moves the logging macros to a separate files for reusability.
    It also adds the arguments to the daemon application

---
+++ include/efimon/logger/macros.hpp
@@ -0,0 +1,45 @@
+/**
+ * @file macros.hpp
+ * @author Luis G. Leon-Vega (luis.leon@ieee.org)
+ * @brief Logger macros
+ *
+ * @copyright Copyright (c) 2024. See License for Licensing
+ */
+
+#ifndef INCLUDE_EFIMON_LOGGER_MACROS_HPP_
+#define INCLUDE_EFIMON_LOGGER_MACROS_HPP_
+
+#include <efimon/logger.hpp>
+#include <iostream>
+#include <memory>
+
+#define EFM_INFO(msg) \
+  { std::cerr << "[INFO]: " << msg << std::endl; }
+#define EFM_WARN(msg) \
+  { std::cerr << "[WARNING]: " << msg << std::endl; }
+#define EFM_WARN_AND_BREAK(msg)                     \
+  {                                                 \
+    std::cerr << "[WARNING]: " << msg << std::endl; \
+    break;                                          \
+  }
+#define EFM_ERROR(msg)                            \
+  {                                               \
+    std::cerr << "[ERROR]: " << msg << std::endl; \
+    return -1;                                    \
+  }
+#define EFM_CHECK(inst, func)                \
+  {                                          \
+    Status s_ = (inst);                      \
+    if (s_.code != Status::OK) func(s_.msg); \
+  }
+#define EFM_CRITICAL_CHECK(inst)                  \
+  {                                               \
+    Status s_ = (inst);                           \
+    if (s_.code != Status::OK) EFM_ERROR(s_.msg); \
+  }
+#define EFM_RES std::cout
+#define LOG_VAL(vars, name, val)   \
+  (vars)[name] = std::make_shared< \
+      Logger::Value<std::remove_reference<decltype(val)>::type>>((val));
+
+#endif /* INCLUDE_EFIMON_LOGGER_MACROS_HPP_ */
---
+++ src/tools/efimon-daemon.cpp
@@ -0,0 +1,115 @@
+/**
+ * @file efimon-meter.cpp
+ * @author Luis G. Leon-Vega (luis.leon@ieee.org)
+ * @brief Tool for metering the consumption of existing processes
+ *
+ * @copyright Copyright (c) 2024. See License for Licensing
+ */
+
+#include <efimon/arg-parser.hpp>
+#include <efimon/logger/macros.hpp>
+
+using namespace efimon;  // NOLINT
+
+static constexpr int kDelay = 3;                  // 3 seconds
+static constexpr uint kDefFrequency = 100;        // 100 Hz
+static constexpr uint kDefaultSampleLimit = 100;  // 100 samples
+static constexpr char kDefaultOutputPath[] = "/tmp";
+static constexpr uint kPort = 5550;
+
+void print_welcome() {
+  std::cout << "-----------------------------------------------------------\n";
+  std::cout << "               EfiMon Daemon Application \n";
+  std::cout << "-----------------------------------------------------------\n";
+}
+
+std::string create_monitoring_file(const std::string &path, const uint pid) {
+  return path + "-" + std::to_string(pid) + ".csv";
+}
+
+int main(int argc, char **argv) {
+  print_welcome();
+  // ------------ Configuration variables ------------
+  uint frequency = kDefFrequency;
+  uint samples = kDefaultSampleLimit;
+  uint delaytime = kDelay;
+  std::string outputpath = kDefaultOutputPath;
+  uint port = kPort;
+
+  // ------------ Arguments ------------
+  ArgParser argparser(argc, argv);
+
+  bool check_frequency =
+      argparser.Exists("-f") || argparser.Exists("--frequency");
+  bool check_samples = argparser.Exists("-s") || argparser.Exists("--samples");
+  bool check_delay = argparser.Exists("-d") || argparser.Exists("--delay");
+  bool check_output =
+      argparser.Exists("-o") || argparser.Exists("--output-folder");
+  bool check_help = argparser.Exists("-h") || argparser.Exists("--help");
+  bool check_port = argparser.Exists("-p") || argparser.Exists("--port");
+
+  if (check_help) {
+    std::string msg =
+        "This application launches a daemon listener for measuring external "
+        "applications: EfiMon Daemon\n\tUsage: "
+        "\n\t";
+    msg += std::string(argv[0]);
+    msg +=
+        " -s,--samples SAMPLES (default: 100). Number of samples to "
+        "collect\n\t\t";
+    msg +=
+        " -o,--output-folder PATH (default: /tmp). Output folder to save "
+        "measurements\n\t\t";
+    msg +=
+        " -f,--frequency FREQUENCY_HZ (default: 100 Hz). Sampling "
+        "frequency\n\t\t";
+    msg +=
+        " -d,--delay DELAY_SECS (default: 3 Secs). Sampling time window\n\t\t";
+    msg +=
+        " -p,--port PORT (default: 5550 Secs). EfiMon Socket Port for "
+        "IPC\n\t\t";
+    msg += " -h,--help: prints this message\n\n";
+    msg +=
+        " \tBy default, the outputs will be saved into the folder with the "
+        "pattern output-pid.csv\n";
+    EFM_ERROR(msg);
+  }
+
+  // ------------ Modify the configurations -----------
+  if (check_samples) {
+    samples =
+        std::stoi(argparser.Exists("-s") ? argparser.GetOption("-s")
+                                         : argparser.GetOption("--samples"));
+  }
+
+  if (check_frequency) {
+    frequency =
+        std::stoi(argparser.Exists("-f") ? argparser.GetOption("-f")
+                                         : argparser.GetOption("--frequency"));
+  }
+
+  if (check_delay) {
+    delaytime =
+        std::stoi(argparser.Exists("-d") ? argparser.GetOption("-d")
+                                         : argparser.GetOption("--delay"));
+  }
+
+  if (check_port) {
+    port = std::stoi(argparser.Exists("-p") ? argparser.GetOption("-p")
+                                            : argparser.GetOption("--port"));
+  }
+
+  if (check_output) {
+    outputpath = argparser.Exists("-o")
+                     ? argparser.GetOption("-o")
+                     : argparser.GetOption("--output-folder");
+  }
+
+  EFM_INFO(std::string("Frequency [Hz]: ") + std::to_string(frequency));
+  EFM_INFO(std::string("Samples: ") + std::to_string(samples));
+  EFM_INFO(std::string("Delay time [secs]: ") + std::to_string(delaytime));
+  EFM_INFO(std::string("Output folder: ") + outputpath);
+  EFM_INFO(std::string("IPC TCP Port: ") + std::to_string(port));
+
+  return 0;
+}
