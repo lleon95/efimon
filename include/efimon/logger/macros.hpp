0e20d1750a2850afa5ffc2aace09eb0703925b73
Luis Leon <lleon95@gmail.com>
2024-10-19 19:06:10 +0200

    Add timestamp to efimon logs
    This adds the timestamp as part of the logging macros

--- include/efimon/logger/macros.hpp
+++ include/efimon/logger/macros.hpp
@@ -9,28 +9,42 @@
 #ifndef INCLUDE_EFIMON_LOGGER_MACROS_HPP_
 #define INCLUDE_EFIMON_LOGGER_MACROS_HPP_
 
+#include <ctime>
 #include <efimon/logger.hpp>
 #include <iostream>
+#include <iterator>
+#include <locale>
 #include <memory>
+#include <string>
+
+[[maybe_unused]] inline std::string GetUTCTimestamp() {
+  // Example of the very popular RFC 3339 format UTC time
+  std::time_t time = std::time({});
+  char time_string[std::size("yyyy-mm-ddThh:mm:ssZ")];  // NOLINT
+  std::strftime(std::data(time_string), std::size(time_string), "%FT%TZ",
+                std::gmtime(&time));
+  std::string str = time_string;
+  return str;
+}
 
 #define EFM_INFO(msg) \
-  { std::cerr << "[INFO]: " << msg << std::endl; }
+  { std::cerr << GetUTCTimestamp() << " [INFO]: " << msg << std::endl; }
 #define EFM_WARN(msg) \
-  { std::cerr << "[WARNING]: " << msg << std::endl; }
-#define EFM_WARN_AND_BREAK(msg)                     \
-  {                                                 \
-    std::cerr << "[WARNING]: " << msg << std::endl; \
-    break;                                          \
+  { std::cerr << GetUTCTimestamp() << " [WARNING]: " << msg << std::endl; }
+#define EFM_WARN_AND_BREAK(msg)                                           \
+  {                                                                       \
+    std::cerr << GetUTCTimestamp() << " [WARNING]: " << msg << std::endl; \
+    break;                                                                \
   }
-#define EFM_ERROR(msg)                            \
-  {                                               \
-    std::cerr << "[ERROR]: " << msg << std::endl; \
-    return -1;                                    \
+#define EFM_ERROR(msg)                                                  \
+  {                                                                     \
+    std::cerr << GetUTCTimestamp() << " [ERROR]: " << msg << std::endl; \
+    return -1;                                                          \
   }
-#define EFM_ERROR_STATUS(msg, code)               \
-  {                                               \
-    std::cerr << "[ERROR]: " << msg << std::endl; \
-    return Status{code, msg};                     \
+#define EFM_ERROR_STATUS(msg, code)                                     \
+  {                                                                     \
+    std::cerr << GetUTCTimestamp() << " [ERROR]: " << msg << std::endl; \
+    return Status{code, msg};                                           \
   }
 #define EFM_CHECK(inst, func)                \
   {                                          \
@@ -52,4 +66,4 @@
   (vars)[name] = std::make_shared< \
       Logger::Value<std::remove_reference<decltype(val)>::type>>((val));
 
-#endif /* INCLUDE_EFIMON_LOGGER_MACROS_HPP_ */
+#endif  // INCLUDE_EFIMON_LOGGER_MACROS_HPP_
